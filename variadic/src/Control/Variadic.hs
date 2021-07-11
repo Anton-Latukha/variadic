6b74b0827ec2453b61ea12333ed02584862a99b4
Anton-Latukha <anton.latukha@gmail.com>
2021-07-11 11:40:10 +0300

    Variadic: org: more sequential/inviting order
    Main idea is that:
    
    Writing code I already several times wrote function `variadic`, that
    converts the functions to variadic ones.
    
    But doing it through type classes only is constraining, it is probably (almost)
    not possible to make general `variadic` maker that accepts any type of function.
    
    That is the main path I always come to variadic stuff. Questioning: is the
    universal `variadic fun` possible.
    
    So I come over to the library & look for function that convers functions to
    variadic.
    
    `ToVariadic` is the class that has `toVariadic` which are created to do that.
    
    Also `To` and then `From` conversions is generally a good way to sequence stuff.
    
    Put utlitarian families after the class, well, again - because people don't need
    to see or be greeted with utilitarian preparation, to spair the details.

--- variadic/src/Control/Variadic.hs
+++ variadic/src/Control/Variadic.hs
@@ -16,6 +16,62 @@ import Control.Monad.Reader (ReaderT(ReaderT))
 import Control.Variadic.Varargs (Varargs(Cons, Nil))
 import Data.Functor (void)
 
+
+-- | Converts a function of arbitrary arity to 'Variadic'.
+class ToVariadic x where
+  toVariadic :: x -> Variadic (ToVariadicArgs x) (ToVariadicReturn x)
+
+instance {-# OVERLAPPING #-}
+  ( ToVariadicArgs a ~ '[]
+  , ToVariadicReturn a ~ a
+  ) => ToVariadic a where
+  toVariadic a = Variadic \_ -> a
+
+instance {-# OVERLAPS #-}
+  ( ToVariadic o
+  , ToVariadicArgs (i -> o) ~ (i ': args)
+  , ToVariadicArgs o ~ args
+  , ToVariadicReturn (i -> o) ~ ToVariadicReturn o
+  ) => ToVariadic (i -> o)
+  where
+  toVariadic f =
+    Variadic \(arg `Cons` args) ->
+      runVariadic (toVariadic (f arg)) args
+
+
+-- | Converts a 'Variadic' to a normal function.
+class FromVariadic args r where
+  fromVariadic :: Variadic args r -> FromVariadicSignature args r
+
+instance FromVariadic '[] a where
+  fromVariadic v = runVariadic v Nil
+
+instance (FromVariadic args a) => FromVariadic (arg ': args) a where
+  fromVariadic v arg =
+    fromVariadic $ Variadic \args ->
+      runVariadic v (arg `Cons` args)
+
+-- | A function whose argument list is collapsed into 'Varargs'
+-- and shows its return type.
+newtype Variadic args a = Variadic
+  { runVariadic :: Varargs args -> a
+  }
+
+-- | Resolves the argument list for a function of arbitrary arity.
+type family ToVariadicArgs x :: [*] where
+  ToVariadicArgs (i -> o) = i ': ToVariadicArgs o
+  ToVariadicArgs a = '[]
+
+-- | Resolves the return type for a function of arbitrary arity.
+type family ToVariadicReturn x :: * where
+  ToVariadicReturn (i -> o) = ToVariadicReturn o
+  ToVariadicReturn a = a
+
+-- | Builds a function signature given the @args@ and return type @r@.
+type family FromVariadicSignature (args :: [*]) (r :: *) :: * where
+  FromVariadicSignature '[] r = r
+  FromVariadicSignature (arg ': args) r = arg -> FromVariadicSignature args r
+
 -- | Same as 'Variadic' but captures the higher-kinded type parameter in the
 -- return type. Useful so we can use 'Monad' and friends with 'Variadic'
 -- functions.
@@ -39,60 +95,6 @@ fromVariadicT
      )
   => VariadicT args m r -> FromVariadicSignature args (m r)
 fromVariadicT = fromVariadic . unVariadicT
-
--- | A function whose argument list is collapsed into 'Varargs'
--- and shows its return type.
-newtype Variadic args a = Variadic
-  { runVariadic :: Varargs args -> a
-  }
-
--- | Resolves the argument list for a function of arbitrary arity.
-type family ToVariadicArgs x :: [*] where
-  ToVariadicArgs (i -> o) = i ': ToVariadicArgs o
-  ToVariadicArgs a = '[]
-
--- | Resolves the return type for a function of arbitrary arity.
-type family ToVariadicReturn x :: * where
-  ToVariadicReturn (i -> o) = ToVariadicReturn o
-  ToVariadicReturn a = a
-
--- | Converts a function of arbitrary arity to 'Variadic'.
-class ToVariadic x where
-  toVariadic :: x -> Variadic (ToVariadicArgs x) (ToVariadicReturn x)
-
-instance {-# OVERLAPPING #-}
-  ( ToVariadicArgs a ~ '[]
-  , ToVariadicReturn a ~ a
-  ) => ToVariadic a where
-  toVariadic a = Variadic \_ -> a
-
-instance {-# OVERLAPS #-}
-  ( ToVariadic o
-  , ToVariadicArgs (i -> o) ~ (i ': args)
-  , ToVariadicArgs o ~ args
-  , ToVariadicReturn (i -> o) ~ ToVariadicReturn o
-  ) => ToVariadic (i -> o)
-  where
-  toVariadic f =
-    Variadic \(arg `Cons` args) ->
-      runVariadic (toVariadic (f arg)) args
-
--- | Builds a function signature given the @args@ and return type @r@.
-type family FromVariadicSignature (args :: [*]) (r :: *) :: * where
-  FromVariadicSignature '[] r = r
-  FromVariadicSignature (arg ': args) r = arg -> FromVariadicSignature args r
-
--- | Converts a 'Variadic' to a normal function.
-class FromVariadic args r where
-  fromVariadic :: Variadic args r -> FromVariadicSignature args r
-
-instance FromVariadic '[] a where
-  fromVariadic v = runVariadic v Nil
-
-instance (FromVariadic args a) => FromVariadic (arg ': args) a where
-  fromVariadic v arg =
-    fromVariadic $ Variadic \args ->
-      runVariadic v (arg `Cons` args)
 
 -- | Convenience constraint enabling variadic.
 --
